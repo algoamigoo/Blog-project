c1ff1a7b04af80b0da84430076413ee720175839
Algoamigoo <itsrudrajani@gmail.com>
2025-07-26 09:13:47 +0530

    first commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import { AppProvider } from "@/context/AppContext";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export const metadata: Metadata = {
+  title: "Blog Project",
+  description: "microservices based blog project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AppProvider>
+          <Navbar />
+          {children}
+        </AppProvider>
+      </body>
+    </html>
+  );
+}
